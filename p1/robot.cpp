7540e8e182620ad68d7161f1a71b1091188ea133
swhitsett <kongzero32@gmail.com>
2014-10-05 15:34:58 -0700

    inital commit

---
+++ p1/robot.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	/**
+		first command line input is the mapfile
+		second is the %error
+		third is the directions ie. NS EW
+		EX...
+		./robot Project1_input1.txt 0.1 SW NS NE W
+		North, South, West, East
+	**/
+
+	return 0;
+}
