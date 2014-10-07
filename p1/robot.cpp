94f6c2aff308c40a52806b197840091e3f742267
swhitsett <kongzero32@gmail.com>
2014-10-07 15:41:46 -0700

    mapComplete

--- p1/robot.cpp
+++ p1/robot.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
 #include <string>
 
 using namespace std;
+
+vector< vector<int> > vector_map;
+double percent_error;
 
 int main(int argc, char *argv[])
 {
@@ -15,37 +19,38 @@ int main(int argc, char *argv[])
 		./robot Project1_input1.txt 0.1 SW NS NE W
 		North, South, West, East
 	**/
-   vector< vector<string> > vector_map;
-   string output;
 
-	ifstream openFile;
-	openFile.open(argv[1]);
-	if(openFile.is_open())
-	{
-		vector<string> x;
-		while(openFile.is_open() && !openFile.eof())
-		{
-			openFile >> output;
-			if(output == "\n")
-			{
-				vector_map.push_back(x);
-				x.clear();
-			}
-			else
-			{
-				x.push_back(output);
-			}
+   ifstream openFile;
+   openFile.open(argv[1]);
+   if(openFile.is_open())
+   {
+   	   vector<int> x;
+   	   string line_of_file;
+
+	   while(openFile.is_open() && !openFile.eof())
+	   {
+		   getline(openFile,line_of_file);
+
+		   istringstream iss(line_of_file);
+		   int number;
+
+		   while(iss >> number)
+		      x.push_back(number);
+
+		   vector_map.push_back(x);
+		   x.clear();
 		}
 		openFile.close();
 	}
-	//openFile >> output; //if newline move to a diffrent vector.
-			//cout<<output<<" ";
-			for(vector< vector<string> >::iterator xt = vector_map.begin(); xt != vector_map.end(); ++xt)
-			{
-				for(vector<string>::iterator yt = xt->begin(); yt != xt->end(); ++yt)
-				{
-					cout<<*yt<<" ";
-				}
-			} 
+	//just for printing
+	for(vector< vector<int> >::iterator xt = vector_map.begin(); xt != vector_map.end(); ++xt)
+	{
+		for(vector<int>::iterator yt = xt->begin(); yt != xt->end(); ++yt)
+		{
+			cout<< *yt<<" ";
+		}
+		cout<<"\n";
+	}
+
 	return 0;
 }
