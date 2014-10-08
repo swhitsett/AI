2e26716e479c8c950a0a0493d530a33c6d66e505
swhitsett <kongzero32@gmail.com>
2014-10-07 22:33:11 -0700

    need transiversity matrix

--- p1/robot.cpp
+++ p1/robot.cpp
@@ -3,15 +3,19 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <stdlib.h> // for atof
+#include <math.h> // for pow
 #include <bitset> // for binary conversion
 
 using namespace std;
 
+double percent_error;
+double errorArray[5];
+vector< vector<int> > vector_map;
+
 string toBinary(int);
 void printArray();
-
-vector< vector<int> > vector_map;
-double percent_error;
+void probOfError(double);
 
 int main(int argc, char *argv[])
 {
@@ -22,7 +26,14 @@ int main(int argc, char *argv[])
 		EX...
 		./robot Project1_input1.txt 0.1 SW NS NE W
 		North, South, West, East
+
+		e = NE or SW
+		epslon = % error 0,1
+		d = number of bits that differ from e (ie 1010 to 1110 diff is 1)
+			-- coresponding value?? the one before?
+
 	**/
+	probOfError(atof(argv[2]));
 
    ifstream openFile;
    openFile.open(argv[1]);
@@ -69,5 +80,16 @@ void printArray()
 			cout<< *yt<<" ";
 		}
 		cout<<"\n";
+	}
+}
+
+void probOfError(double error)
+{
+	double result;
+
+	for(int i=0;i<5;i++)
+	{
+		errorArray[i] = pow(error, i)*pow((1 - error),(4-i));
+		cout<<errorArray[i]<<endl;
 	}
 }
