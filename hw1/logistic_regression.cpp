c7568bf5369465e3cc95f670918b17e890cf15eb
swhitsett <kongzero32@gmail.com>
2014-11-03 16:29:58 -0800

    inital commit hw1

---
+++ hw1/logistic_regression.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <time.h>
+
+using namespace std;
+vector< vector<int> > inputMatrix;
+vector<int> classLables;
+vector<double> initalWeights;
+double alpha = 0.01;
+int itterations = 0;
+
+int main(int argc, char* argv[])
+{
+	ifstream openFile;
+	openFile.open(argv[1]);
+	if(openFile.is_open())
+	{
+		   vector<int> x;
+		   string line;
+
+	   while(!openFile.eof())
+	   {
+		   getline(openFile,line);
+
+		   istringstream iss(line);
+		   int number;
+		   
+		   x.push_back(1);
+		   while(iss >> number)
+		      x.push_back(number);
+
+		   inputMatrix.push_back(x);
+
+		   x.clear();
+		}
+		openFile.close();
+	}
+
+	openFile.open(argv[2]);
+	if(openFile.is_open())
+	{
+		while(!openFile.eof())
+		{
+			int i = 0;
+			openFile >> i;
+			classLables.push_back(i);
+		}
+
+		openFile.close();
+	}
+
+	itterations = atoi(argv[3]);
+
+	for(int i=0;i<4;i++)
+		initalWeights.push_back(1.0);
+	
+	for(int i=0;i<itterations;i++)
+	{
+		double wxVariable = 0.0;
+		double hVariable = 0.0;
+
+		srand(time(NULL));
+		int randChoice = rand()% inputMatrix.size() + 0;
+
+		wxVariable = ( initalWeights[0]*inputMatrix[randChoice][0] + 
+					   initalWeights[1]*inputMatrix[randChoice][1] +
+					   initalWeights[2]*inputMatrix[randChoice][2] + 
+					   initalWeights[3]*inputMatrix[randChoice][3] );
+
+		hVariable = 1 / (1 + exp(-wxVariable));
+
+		double weight = 0.0;
+
+		for(int i=0;i<4;i++)
+		{
+			initalWeights[i] = initalWeights[i] + alpha *
+							   ((double)classLables[i] - wxVariable) * 
+							   wxVariable * (1.0 - wxVariable) * 
+							   (double)inputMatrix[randChoice][i];
+		}
+	}
+	for(int i=0;i<4;i++)
+		cout<<initalWeights[i]<<endl;
+	//cout<<inputMatrix[0][0]<<endl;
+	// for(vector< vector<int> >::iterator yt = inputMatrix.begin(); yt != inputMatrix.end(); ++yt)
+	// {
+	// 	for(vector<int>::iterator xt = yt->begin(); xt != yt->end(); ++xt)
+	// 	{
+	// 		cout<< *xt<<" ";
+	// 	}
+	// 	cout<<"\n";
+	// }
+	
+
+	return 0;
+}
