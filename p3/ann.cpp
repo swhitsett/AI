4c210478790ddbb7796ec215ba84627fdead6b94
swhitsett <kongzero32@gmail.com>
2014-11-20 02:12:58 -0800

    somthing

--- p3/ann.cpp
+++ p3/ann.cpp
@@ -13,6 +13,7 @@ vector< vector<double> > out_encoding;
 vector< vector<double> > train_input;
 vector< vector<double> > test_input;
 vector< vector<double> > weights;
+vector< vector<double> > neural_network;
 vector< int > train_output;
 vector< int > test_output;
 vector< int > structure_input;
@@ -21,6 +22,7 @@ int itterations;
 void recordMatrix(ifstream&, vector< vector<double> >&);
 void recordArray(ifstream&, vector< int >&);
 void createEncoding(vector< vector<double> >&);
+void run();
 
 int main (int argc, char *argv[])
 {
@@ -50,8 +52,55 @@ int main (int argc, char *argv[])
       itterations = atoi(argv[7]);
       //create encoding
       createEncoding(out_encoding);
+      //run back propragation
+      run();
    } 
    return 0;
+}
+void run()
+{
+   double new_weight;
+   int struct_postion = 0;
+   for(int itter; itter<itterations; itter++)
+   {
+      for (int tRow=0; tRow<train_input.size()-1; tRow++)                  //tRow = move y in trainI
+      {
+         for(int i=1; i<structure_input.size(); i++)                       //i = move throught structure *
+         {
+            for(int j=0; j<structure_input[i]; j++)
+            {
+               neural_network[i][j] = asdfasfd
+            }
+            // for(int j=0; j<structure_input[i]; j++)                        //j = move through nodes in hidden layer top>down
+            // {
+            //    for(int k=0; k<train_input.at(1).size(); k++)               //k = move x in trainI
+            //    {
+            //       for(int l=struct_postion; l<structure_input[i-1]; l++)   //l = move y in weights
+            //       {
+            //          for(int m=0; m<weights.at(l).size(); m++)             //m = move x in weights
+            //          {
+            //             new_weight = new_weight + (train_input[tRow][k] * weights[l][m]);
+            //             // calculated_layer_values.at(i-1).push_back( new_weight );
+            //          }
+            //          calculated_layer_values.push_back( 0.01 + new_weight );
+            //       }
+            //       struct_postion = struct_postion + structure_input[i-1];
+            //    }
+            // }
+         }
+      }
+   }
+   // for(int i=0; i<train_input.size()-1; i++)         //i = train input y
+   // {
+   //    for(int j=0;j<structure[x];j++)  //j = train input x
+   //    {
+   //       for(int k=0;k<structure.at(j).size();k++)   //k = weights itterator
+   //       {
+
+   //          calculated_layer_values[point_of_structure][layer] = calculated_layer_values[][] + (train_input[i][j] * weights[j][k])
+   //       }
+   //    }
+   // }
 }
 void recordArray(ifstream& file, vector< int >& vec)
 {
